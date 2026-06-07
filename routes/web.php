b3ba2a3d28ef3a7681053f67384af993924abf0a
hatimi14 <hatim.kenfaui@inslapineda.cat>
2026-06-07 20:31:11 +0200

    arreglao

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,20 @@
 
 use App\Http\Controllers\PageController;
 
-Route::get('/', [PageController::class, 'login']);
+//Route::get('/', [PageController::class, 'login']);
+Route::get('/', function () {
+    return view('login');
+});
 
-Route::get('/dashboard', [PageController::class, 'dashboard']);
-Route::get('/editarProyecto/{id}', [PageController::class, 'editarProyecto']);
-Route::get('/crearProyecto', [PageController::class, 'crearProyecto']);
+//Route::get('/dashboard', [PageController::class, 'dashboard']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+//Route::get('/editarProyecto/{id}', [PageController::class, 'editarProyecto']);
+Route::get('/editarProyecto/{id}', function ($id) {
+    return view('editarProyecto' , ['id' => $id]);
+});
+//Route::get('/crearProyecto', [PageController::class, 'crearProyecto']);
+Route::get('/crearProyecto', function () {
+    return view('crearProyecto');
+});
